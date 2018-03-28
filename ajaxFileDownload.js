f53eded9b46e19d81c107c390105a4da7077178d
LittleProgrammer007 <magicapprentice@163.com>
2018-03-28 23:13:14 +0800

    commet

--- ajaxFileDownload.js
+++ ajaxFileDownload.js
@@ -1,4 +1,7 @@
-function fileDownload(fileUrl, errorCallback) {
+//通过一个隐藏的iframe发起GET请求下载文件，response中需要设置cookie，根据cookie的值判断是成功还是失败。
+//服务器需要支持iframe请求，需要设置X-Frame-Options。如果请求被拒绝，则会报错。
+
+function fileDownload(fileUrl, successCallback, errorCallback) {
     //create a temporary iframe that is used to request the fileUrl as a GET request
     var $iframe = $("<iframe>").hide().prop("src", fileUrl).appendTo("body");
 
@@ -11,6 +14,7 @@ function fileDownload(fileUrl, errorCallback) {
         if (document.cookie.toLowerCase().indexOf("filedownload=true") > -1) {
 
             //execute specified success callback
+            successCallback();
 
             //remove cookie
             document.cookie = "fileDownload=; path=/; expires=" + new Date(0).toUTCString() + ";";
@@ -19,20 +23,18 @@ function fileDownload(fileUrl, errorCallback) {
             $iframe.remove();
             return;
         }
-        
+
         if (document.cookie.toLowerCase().indexOf("filedownload=false") > -1) {
-        	
-        	//execute specified fail callback
-        	showAlert('danger', 'ERROR!', 'Not Found');
-        	
-        	//remove cookie
-        	document.cookie = "fileDownload=; path=/; expires=" + new Date(0).toUTCString() + ";";
-        	if(isFunction(errorCallback)){
-        		errorCallback($iframe);
-        	}
-        	//remove iframe 
-        	$iframe.remove();
-        	return;
+
+            //execute specified fail callback
+            errorCallback();
+
+            //remove cookie
+            document.cookie = "fileDownload=; path=/; expires=" + new Date(0).toUTCString() + ";";
+
+            //remove iframe 
+            $iframe.remove();
+            return;
         }
 
         //has an error occured?
